808ec8150058befb2bdfc987ab2565fe0c345752
shadow0wolf <phreak911x@gmail.com>
2023-10-03 03:52:46 +0530

    fix1

--- config_test.go
+++ config_test.go
@@ -12,43 +12,34 @@ import (
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
-/*
-func TestLoadConfig2(t *testing.T) {
-
-	//factories, err := componenttest.ExampleComponents()
-	assert.NoError(t, err)
-
-	//factories.Extension[typeStr] = NewFactory()
-
-	//cfg, err := configtest.LoadConfigFile(t, path.Join(".", "testdata", "config.yaml"), factories)
-	require.NoError(t, err)
-	//require.NotNil(t, cfg)
-}
-*/
-
 type Aaa struct {
-	id          component.ID
-	expected    component.Config
-	expectedErr bool
+	id       component.ID
+	expected component.Config
 }
 
 func TestLoadConfig(t *testing.T) {
 	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 	require.NoError(t, err)
 	factory := NewFactory()
-	cfg := factory.CreateDefaultConfig()
+	defaultCfg := factory.CreateDefaultConfig()
 
 	var tt = Aaa{
 		id: component.NewID(metadata.Type),
 		expected: &Config{
-			ValidationURL: "https://pod.ics.dev:444/session-service/api/v1/session/Agent",
-			Headerkey:     "IDS-AGENT-SESSION-ID",
+			TimeOut:            2,
+			ClientSideSsl:      true,
+			ValidationURL:      "https://pod.ics.dev:444/session-service/api/v1/session/Agent",
+			Headerkey:          "IDS-AGENT-SESSION-ID",
+			ClientCertPath:     "/mnt/a/c1Client.crt",
+			CACertPath:         "/mnt/a/ca_cert_path.crt",
+			InsecureSkipVerify: true,
+			ClientKeyPath:      "/mnt/a/client_key_path.pem",
 		},
 	}
 
 	sub, err := cm.Sub(tt.id.String())
 	require.NoError(t, err)
-	require.NoError(t, component.UnmarshalConfig(sub, cfg))
-	assert.NoError(t, component.ValidateConfig(cfg))
-	assert.Equal(t, tt.expected, cfg)
+	require.NoError(t, component.UnmarshalConfig(sub, defaultCfg))
+	assert.NoError(t, component.ValidateConfig(defaultCfg))
+	assert.Equal(t, tt.expected, defaultCfg)
 }
--- extension.go
+++ extension.go
@@ -7,16 +7,14 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"crypto/x509"
 	"github.com/gofiber/fiber/v2/log"
 	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/extension/auth"
 	"go.uber.org/zap"
-)
-
-var (
-	errGenericError     = errors.New("errGenericError")
-	errNotAuthenticated = errors.New("authentication didn't succeed")
+	"os"
+	"time"
 )
 
 type infaAuthExtension struct {
@@ -25,10 +23,15 @@ type infaAuthExtension struct {
 }
 
 func newExtension(ctx context.Context, cfg *Config, logger *zap.Logger) (auth.Server, error) {
-	if cfg.ValidationURL == "" {
-		return nil, errGenericError
-	}
+	/*
+		if cfg.ValidationURL == "" {
+			return nil, errors.New("validation url is empty")
+		}
 
+		if cfg.Headerkey == "" {
+			return nil, errors.New("header key is empty")
+		}
+	*/
 	oe := &infaAuthExtension{
 		cfg:    cfg,
 		logger: logger,
@@ -36,27 +39,76 @@ func newExtension(ctx context.Context, cfg *Config, logger *zap.Logger) (auth.Se
 	return auth.NewServer(auth.WithServerStart(oe.start), auth.WithServerAuthenticate(oe.authenticate)), nil
 }
 
+// function that executes to initialize the auth extension , returns no error and nil in case of success
+// associated to Start()
 func (e *infaAuthExtension) start(context.Context, component.Host) error {
-	/*
-		provider, err := getProviderForConfig(e.cfg)
-		if err != nil {
-			return fmt.Errorf("failed to get configuration from the auth server: %w", err)
-		}
-		e.provider = provider
+	log.Debug("begin executing extension.start")
 
-		e.verifier = e.provider.Verifier(&oidc.Config{
-			ClientID: e.cfg.Audience,
-		})
+	//validation url and header key must NOT be empty
+	if e.cfg.ValidationURL == "" {
+		return errors.New("ValidationURL is empty")
+	}
+
+	if e.cfg.Headerkey == "" {
+		return errors.New("Headerkey is empty")
+	}
+
+	/*
+		if e.cfg.CACertPath == "" {
+			log.Debug("CACertPath is empty")
+			return errors.New("CACertPath is empty")
+		}
+
+		log.Debugf("CACertPath is : %s ", e.cfg.CACertPath)
+		_, error := os.Stat(e.cfg.CACertPath)
+		if error != nil {
+			log.Debugf("error reading CACertPath")
+			return errors.New("error reading CACertPath")
+		}
 	*/
+
+	//validate client cert
+	log.Debugf("ClientSideSsl is : %s ", e.cfg.ClientSideSsl)
+	if e.cfg.ClientSideSsl {
+		log.Debugf("ClientCertPath is : %s ", e.cfg.ClientCertPath)
+		if e.cfg.ClientCertPath == "" {
+			log.Debug("ClientCertPath is empty")
+			return errors.New("ClientCertPath is empty")
+
+		} else {
+			_, error := os.Stat(e.cfg.ClientCertPath)
+			if error != nil {
+				log.Debugf("error reading ClientCert")
+				return errors.New("error reading ClientCert")
+			}
+		}
+
+		log.Debugf("ClientKeyPath is : %s ", e.cfg.ClientKeyPath)
+		if e.cfg.ClientKeyPath == "" {
+			log.Debug("ClientKeyPath is empty")
+			return errors.New("ClientKeyPath is empty")
+
+		} else {
+			_, error := os.Stat(e.cfg.ClientKeyPath)
+			if error != nil {
+				log.Debugf("error reading ClientKeyPath")
+				return errors.New("error reading ClientKeyPath")
+			}
+		}
+
+	}
+
+	log.Debug("finished executing extension.start")
 	return nil
 }
 
 // authenticate checks whether the given context contains valid auth data. Successfully authenticated calls will always return a nil error and a context with the auth data.
+// this associated to the Authenticate() method
 func (e *infaAuthExtension) authenticate(ctx context.Context, headers map[string][]string) (context.Context, error) {
-	log.Debug("executing authenticate")
-	log.Debugf("headers ", headers)
-	log.Debugf("ctx ", ctx)
-	log.Debug("e.cfg ", *e.cfg)
+	log.Debug("executing extensions.authenticate() ")
+	log.Debugf("headers :: %A", headers)
+	log.Debugf("ctx :: %A", ctx)
+	log.Debugf("e.cfg :: %A", *e.cfg)
 
 	var h []string
 
@@ -69,67 +121,104 @@ func (e *infaAuthExtension) authenticate(ctx context.Context, headers map[string
 			h = headers["ids-agent-session-id"]
 			if h == nil {
 				log.Debug("ids-agent-session-id header is null")
-				return ctx, errGenericError
+				return ctx, errors.New("ids-agent-session-id header does not exist")
 			}
 		}
 	}
 
 	token := h[0]
-	log.Debug("token is :" + token)
-	if len(e.cfg.ValidationURL) == 0 {
-		return ctx, errGenericError
-	}
+	log.Debugf("token is %A", token)
 
 	if len(token) == 0 {
-		return ctx, errGenericError
+		return ctx, errors.New("ids-agent-session-id header value is empty string")
 	}
 
 	cl := client.FromContext(ctx)
-	cl.Auth = &authData{
-		token: token,
+	status, err := validateToken(e.cfg, token)
+	if err != nil || status == false {
+		return ctx, err
 	}
 
-	status := false
-	status = validateToken(e.cfg.ValidationURL, token, e.cfg.Headerkey)
-	if !status {
-		return ctx, errNotAuthenticated
-	}
+	//success
 	return client.NewContext(ctx, cl), nil
 }
 
-func validateToken(url string, sessionToken string, headerKey string) bool {
+// this method creates a HTTP client and makes HTTP/S request to session service, if http status is 200 , it returns
+// true with nil error , otherwise non-nil error is returned
+func validateToken(cfg *Config, sessionToken string) (bool, error) {
 	// Create an HTTP client
-	log.Debug("calling validateToken ")
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	log.Debug("calling extension.validateToken() ")
+
+	tr := &http.Transport{}
+
+	//read ca-cert file
+	pool := x509.NewCertPool()
+	if cfg.CACertPath != "" {
+		cert, err := ioutil.ReadFile(cfg.CACertPath)
+		if err != nil {
+			log.Debugf("Error reading CA certificate: %A", err)
+			return false, err
+		} else {
+			pool.AppendCertsFromPEM(cert)
+			tr = &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: cfg.InsecureSkipVerify, RootCAs: pool},
+			}
+		}
+	} else {
+		tr = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: cfg.InsecureSkipVerify},
+		}
 	}
-	// Create a client with the custom transport
-	client := &http.Client{Transport: tr}
 
-	//client := &http.Client{}
+	// Load client certificate and private key
+	if cfg.ClientSideSsl {
+		cert, err := tls.LoadX509KeyPair(cfg.ClientCertPath, cfg.ClientKeyPath)
+		if err != nil {
+			log.Debugf("Error loading client certificate:", err)
+			return false, err
+		}
+		config := &tls.Config{
+			Certificates:       []tls.Certificate{cert},
+			RootCAs:            pool,
+			InsecureSkipVerify: cfg.InsecureSkipVerify,
+		}
+		tr = &http.Transport{
+			TLSClientConfig: config,
+		}
+	}
 
-	// Create a GET request
+	client := &http.Client{}
+	if cfg.TimeOut > 0 {
+		client = &http.Client{
+			Timeout:   time.Duration(cfg.TimeOut) * time.Second,
+			Transport: tr}
+	} else {
+		client = &http.Client{
+			Timeout:   2 * time.Second,
+			Transport: tr}
+	}
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", cfg.ValidationURL, nil)
 	if err != nil {
 		log.Debugf("Error creating request:", err)
-		return false
+		return false, err
 	}
-	//req.Header.Add("IDS-AGENT-SESSION-ID", sessionToken)
-	req.Header.Add(headerKey, sessionToken)
+
+	req.Header.Add(cfg.Headerkey, sessionToken)
 
 	// Make the request
-	log.Debug("calling making http request ")
+	log.Debugf("invoking http request : %A", req)
 	resp, err := client.Do(req)
-	log.Debugf("resp ", resp)
+	log.Debugf("got response %A", resp)
 
 	if err != nil {
 		log.Debugf("Error making request:", err)
-		return false
+		return false, err
 	}
+
 	if resp.StatusCode != 200 {
-		log.Debugf("http status is not 200:")
-		return false
+		log.Debug("http status is not 200 in response")
+		return false, errors.New("http status is not 200 in response")
 	}
 
 	defer resp.Body.Close()
@@ -138,11 +227,11 @@ func validateToken(url string, sessionToken string, headerKey string) bool {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Debugf("Error reading response body:", err)
-		return false
+		return false, err
 	}
 
 	// Print the response
-	log.Debugf(string(body))
-	return true
+	log.Debugf("response body is %A", string(body))
+	return true, nil
 
 }
